c89daba1df190a75857bee3c9d744d045589fc2e
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-01-27 23:24:32 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 def rescue_princess_peach
  status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+=======
+def 
+puts "Hooray! Mario has rescued Princess Peach."
+>>>>>>> fb7f892df63bb24b8b656ea6b5dd5d56a37c498e
 end
 
 ## Comment the below back in to see method scope in action!
